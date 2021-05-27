a35b06c243a4f597b73714653eb8b40e00a55b42
charliewhyman <65013284+charliewhyman@users.noreply.github.com>
2021-05-27 13:31:05 +0100

    Changed export filetype from csv to xlsx
    Added an xlsxwriter instance to write the dataframe to an .xlsx file, for easier analysis by users.

--- new-TRICS.py
+++ new-TRICS.py
@@ -57,10 +57,35 @@ trip_rates = trip_rates[~trip_rates.time_range.str.contains('Count Type:')]
 #reset index
 trip_rates = trip_rates.reset_index(drop=True)
 
-#export to csv
+#export to xlsx
 Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
-output_csv = asksaveasfilename(title="Choose where to save trip rate csv", defaultextension=".csv", filetypes=(("csv file", "*.csv"),)) # show an "Save" dialog box and return the path to the selected file
-print(output_csv)
+output_file = asksaveasfilename(title="Choose where to save .xlsx", defaultextension=".xlsx", filetypes=(("xlsx file", "*.xlsx"),)) # show an "Save" dialog box and return the path to the selected file
+print(output_file)
+print(trip_rates)
 
-print("Exporting to "+output_csv)
-trip_rates.to_csv(output_csv, index=False)
+print("Exporting to "+output_file)
+writer = pd.ExcelWriter(output_file, engine='xlsxwriter')
+trip_rates.to_excel(writer, sheet_name='Trip rates', startrow=1, header=False, index=False)
+
+# Get the xlsxwriter workbook and worksheet objects.
+workbook = writer.book
+worksheet = writer.sheets['Trip rates']
+
+# Get the dimensions of the dataframe.
+(max_row, max_col) = trip_rates.shape
+
+# Create a list of column headers, to use in add_table().
+column_settings = [{'header': column} for column in trip_rates.columns]
+
+# Add the Excel table structure. Pandas will add the data.
+worksheet.add_table(0, 0, max_row, max_col - 1, {'columns': column_settings})
+
+# Make the columns wider for clarity.
+worksheet.set_column(0, max_col - 1, 12)
+
+# Close the Pandas Excel writer and output the Excel file.
+writer.save()
+
+#create trip rate graphs
+
+
